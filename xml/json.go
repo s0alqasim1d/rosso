8fd4d5f53f0931cf3d3c9d1db19211a6fedae2eb
Steven Penny <srpen6+4cq2@gmail.com>
2023-04-06 23:09:53 -0500

    deleted:    xml/indent.go

--- xml/json.go
+++ xml/json.go
@@ -6,6 +6,36 @@ import (
    "io"
 )
 
+func decoder(data []byte) *xml.Decoder {
+   dec := xml.NewDecoder(bytes.NewReader(data))
+   dec.AutoClose = xml.HTMLAutoClose
+   dec.Strict = false
+   return dec
+}
+
+type Scanner struct {
+   Data []byte
+   Sep []byte
+}
+
+func (s Scanner) Decode(val any) error {
+   data := append(s.Sep, s.Data...)
+   dec := decoder(data)
+   for {
+      _, err := dec.Token()
+      if err != nil {
+         high := dec.InputOffset()
+         return decoder(data[:high]).Decode(val)
+      }
+   }
+}
+
+func (s *Scanner) Scan() bool {
+   var found bool
+   _, s.Data, found = bytes.Cut(s.Data, s.Sep)
+   return found
+}
+
 func Indent(dst io.Writer, src io.Reader, prefix, indent string) error {
    decode := xml.NewDecoder(src)
    encode := xml.NewEncoder(dst)

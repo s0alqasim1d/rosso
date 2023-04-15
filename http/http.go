89ad522941d7c5ea3e244b06c6c480150d46ec54
Steven Penny <srpen6+4cq2@gmail.com>
2023-04-15 11:12:11 -0500

    new file:   http/request.go

--- http/http.go
+++ http/http.go
@@ -4,14 +4,68 @@ import (
    "2a.pages.dev/rosso/strconv"
    "bufio"
    "bytes"
+   "fmt"
    "io"
    "net/http"
+   "net/http/httputil"
    "net/textproto"
    "net/url"
    "os"
    "strings"
    "time"
 )
+
+const Status_Found = http.StatusFound
+
+type Client struct {
+   Log_Level int // this needs to work with flag.IntVar
+   Status int
+   http.Client
+}
+
+var Default_Client = Client{
+   Client: http.Client{
+      CheckRedirect: func(*http.Request, []*http.Request) error {
+         return http.ErrUseLastResponse
+      },
+   },
+   Log_Level: 1,
+   Status: http.StatusOK,
+}
+
+func (c Client) Do(req *Request) (*Response, error) {
+   switch c.Log_Level {
+   case 1:
+      fmt.Println(req.Method, req.URL)
+   case 2:
+      dump, err := httputil.DumpRequest(req.Request, true)
+      if err != nil {
+         return nil, err
+      }
+      enc := strconv.Encode(dump)
+      if strings.HasSuffix(enc, "\n") {
+         fmt.Print(enc)
+      } else {
+         fmt.Println(enc)
+      }
+   }
+   res, err := c.Client.Do(req.Request)
+   if err != nil {
+      return nil, err
+   }
+   if res.StatusCode != c.Status {
+      return nil, fmt.Errorf(res.Status)
+   }
+   return res, nil
+}
+
+func (c Client) Get(ref string) (*Response, error) {
+   req, err := Get_URL(ref)
+   if err != nil {
+      return nil, err
+   }
+   return c.Do(req)
+}
 
 func Read_Request(r *bufio.Reader) (*http.Request, error) {
    var req http.Request
@@ -103,61 +157,6 @@ func (p *Progress) Write(data []byte) (int, error) {
    write, err := p.w.Write(data)
    p.bytes_written += write
    return write, err
-}
-
-type Request struct {
-   *http.Request
-}
-
-func Get() *Request {
-   return New_Request(http.MethodGet, new(url.URL))
-}
-
-func Get_URL(ref string) (*Request, error) {
-   href, err := url.Parse(ref)
-   if err != nil {
-      return nil, err
-   }
-   return New_Request(http.MethodGet, href), nil
-}
-
-func New_Request(method string, ref *url.URL) *Request {
-   req := http.Request{
-      Header: make(http.Header),
-      Method: method,
-      ProtoMajor: 1,
-      ProtoMinor: 1,
-      URL: ref,
-   }
-   return &Request{&req}
-}
-
-func Patch() *Request {
-   return New_Request(http.MethodPatch, new(url.URL))
-}
-
-func Post() *Request {
-   return New_Request(http.MethodPost, new(url.URL))
-}
-
-func Post_URL(ref string) (*Request, error) {
-   href, err := url.Parse(ref)
-   if err != nil {
-      return nil, err
-   }
-   return New_Request(http.MethodPost, href), nil
-}
-
-func Put() *Request {
-   return New_Request(http.MethodPut, new(url.URL))
-}
-
-func (r Request) Body_Bytes(b []byte) {
-   r.Body = io.NopCloser(bytes.NewReader(b))
-}
-
-func (r Request) Body_String(s string) {
-   r.Body = io.NopCloser(strings.NewReader(s))
 }
 
 type Response = http.Response
